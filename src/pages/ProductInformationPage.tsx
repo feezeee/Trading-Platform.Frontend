84561960160d868045217b1689afc412e3dcdfff
Denis Skurat <denis.skurat@effective-soft.com>
2023-05-09 00:21:20 +0300

    some fixes

--- src/pages/ProductInformationPage.tsx
+++ src/pages/ProductInformationPage.tsx
@@ -1,58 +1,182 @@
+import ProductEditMenu, {
+  UpdateProductValues,
+} from "../components/product_menu/ProductEditMenu";
 import React, { useEffect, useState } from "react";
 
 import { CategoryService } from "../core/services/CategoryService";
 import { GetCategoryEntity } from "../core/entities/category/GetCategoryEntity";
+import { GetFullUserEntity } from "../core/entities/user/GetFullUserEntity";
 import { GetProductEntity } from "../core/entities/product/GetProductEntity";
+import { ImageService } from "../core/services/ImageService";
+import ModalWithTwoButtons from "../components/modals/ModalWithTwoButtons";
 import MyContainer from "../components/containers/MyContainer";
 import ProductInfMenu from "../components/product_menu/ProductInfMenu";
 import { ProductService } from "../core/services/ProductService";
+import { PutProductEntity } from "../core/entities/product/PutProductEntity";
+import localStorageKeys from "../core/localStorageKeys";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router";
 
 export interface IProductInformationPageProps {}
 
-const ProductInformationPage: React.FunctionComponent<IProductInformationPageProps> = (props) => {
+const ProductInformationPage: React.FunctionComponent<
+  IProductInformationPageProps
+> = (props) => {
   const { id } = useParams();
+
   const [isMyContainerLoading, setIsMyContainerLoading] = useState(true);
-
   const [product, setProduct] = useState<GetProductEntity | null>(null);
-
   const [categories, setCategories] = useState<GetCategoryEntity[]>([]);
+
+  const [user, setUser] = useState<GetFullUserEntity | null>(null);
 
   const productService = new ProductService();
   const categoryService = new CategoryService();
+  const imageService = new ImageService();
+
+  const navigate = useNavigate();
+
+  const fetchCategories = async () => {
+    setCategories(await categoryService.getCategories());
+  };
+
+  const fetchProducts = async () => {
+    setProduct(await productService.getProductById(id!));
+  };
+
+  const fetchUserFromLocalStorage = () => {
+    const userJson = localStorage.getItem(localStorageKeys.user);
+    const userLocalStorage: GetFullUserEntity | null =
+      userJson === null ? null : JSON.parse(userJson);
+    setUser(userLocalStorage);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      setProduct(await productService.getProductById(id ?? ""));
-      setCategories(await categoryService.getCategories());
+    const fetch = async () => {
+      setIsMyContainerLoading(true);
+      if (id === undefined) {
+        navigate("/products");
+        return;
+      }
+      await fetchProducts();
+      await fetchCategories();
+      fetchUserFromLocalStorage();
       setIsMyContainerLoading(false);
     };
-    setIsMyContainerLoading(true);
-    fetchData();
+    fetch()
   }, []);
+
+  const [isEditable, setIsEditable] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [showDeletingLoading, setShowDeletingLoading] = useState(false);
+
+  const deleteProduct = async () => {
+    setShowDeletingLoading(true);
+    if (product === null) {
+      setShowDeletingLoading(false);
+      return;
+    }
+    const response = await productService.deleteProduct(product.id);
+    navigate("/products");
+    setShowDeletingLoading(false);
+  };
+
+  const updateProduct = async (updateProduct: UpdateProductValues) => {
+    setIsMyContainerLoading(true);
+    let newImagesUrls: string[] = updateProduct.imageUrlArr;
+    for (let i = 0; i < updateProduct.imageFileArr.length; i++) {
+      const response = await imageService.uploadImage(
+        updateProduct.imageFileArr[i],
+        null
+      );
+      if (response != null) {
+        newImagesUrls = [...newImagesUrls, response.imageUrl];
+      }
+    }
+
+    const putProduct: PutProductEntity = {
+      id: updateProduct.id,
+      name: updateProduct.name,
+      price: updateProduct.price,
+      categories: updateProduct.categoryIdArr,
+      description: updateProduct.description,
+      phoneNumbers: updateProduct.phoneNumberArr,
+      images: newImagesUrls,
+    };
+    const product = await productService.updateProduct(putProduct);
+    setProduct(product);
+    setIsMyContainerLoading(false);
+    setIsEditable(false);
+  };
 
   return (
     <MyContainer
       onSearch={() => {}}
       isLoading={isMyContainerLoading}
       searchFieldIsHidden={true}
+      onLogout={(status) => {
+        navigate("/products");
+      }}
     >
       <div className="d-flex">
         <div className="container-fluid p-3">
-          {isMyContainerLoading === true ? (
-            <ProductInfMenu
-              categories={[]}
-              showWriteButton={true}
-            />
-          ) : (
-            product != null && (
+          <ModalWithTwoButtons
+            text="Вы уверены, что хотите удалить?"
+            cancelText="Нет"
+            submitText="Да"
+            modalShow={isDelete}
+            onCancel={() => setIsDelete(false)}
+            onSubmit={deleteProduct}
+            showLoading={showDeletingLoading}
+          />
+          <div className="d-flex flex-column">
+            <div className="d-flex justify-content-end">
+              <div className="btn-group" role="group">
+                {user !== null &&
+                  product !== null &&
+                  (user.roles.find((item) => item.name === "admin") !==
+                    undefined ||
+                    product.userId === user.id) && [
+                    isEditable === false && (
+                      <button
+                        onClick={() => setIsEditable(true)}
+                        className="btn btn-outline-success"
+                      >
+                        <div className="d-flex justify-content-center align-items-center">
+                          <i className="bi bi-pencil"></i>
+                        </div>
+                      </button>
+                    ),
+                    <button
+                      onClick={() => setIsDelete(true)}
+                      className="btn btn-outline-danger"
+                    >
+                      <div className="d-flex justify-content-center align-items-center">
+                        <i className="bi bi-trash"></i>
+                      </div>
+                    </button>,
+                  ]}
+              </div>
+            </div>
+            {product === null && (
+              <ProductInfMenu categories={[]} showWriteButton={false} />
+            )}
+            {product !== null && isEditable === false && (
               <ProductInfMenu
+                categories={categories}
+                product={product}
+                showWriteButton={false}
+              />
+            )}
+            {product !== null && isEditable === true && (
+              <ProductEditMenu
                 product={product}
                 categories={categories}
-                showWriteButton={true}
+                onSave={updateProduct}
+                onCancel={() => setIsEditable(false)}
               />
-            )
-          )}
+            )}
+          </div>
         </div>
       </div>
     </MyContainer>
